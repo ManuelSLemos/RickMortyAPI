4b0d494dc569caba3913a65c023956de8db270e5
Manuel S. Lemos <m.lemos@geekshubs.com>
2020-11-26 12:15:28 +0100

    setState from api

--- src/containers/CharacterList/CharacterList.jsx
+++ src/containers/CharacterList/CharacterList.jsx
@@ -5,12 +5,20 @@ import axios from 'axios';
 class CharacterList extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            character: []
+        }
     }
 
     componentDidMount(){
         axios.get('https://rickandmortyapi.com/api/character')
-            .then( api => console.log(api.data.results))
+            .then( api => {
+                console.log(api.data.results)
+                this.setState({ character: api.data.results })
+            })
             .catch( err => console.log(err));
+        
     }
 
     render(){
