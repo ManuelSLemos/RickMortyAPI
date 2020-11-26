0e15926871671b3106c987acb9473f46d9f8259f
Manuel S. Lemos <m.lemos@geekshubs.com>
2020-11-26 11:54:52 +0100

    Config Axios

--- src/containers/CharacterList/CharacterList.jsx
+++ src/containers/CharacterList/CharacterList.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 class CharacterList extends Component {
     constructor(props){
         super(props);
+    }
+
+    componentDidMount(){
+        axios.get('https://rickandmortyapi.com/api/character')
+            .then( api => console.log(api.data.results))
+            .catch( err => console.log(err));
     }
 
     render(){
