36270f8652c96ca2d25cc0f9318dd55d3018f90d
Manuel S. Lemos <m.lemos@geekshubs.com>
2020-11-30 12:55:56 +0100

    Create Search and More View

--- src/containers/CharacterList/CharacterList.jsx
+++ src/containers/CharacterList/CharacterList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import axios from 'axios';
 
@@ -12,19 +12,26 @@ class CharacterList extends Component {
 
         this.state = {
             characters: [],
-            page: 1
+            search: [],
+            page: 1,
+            text: ''
         }
     }
 
-    componentDidMount(){
-        this.getCharacterServices(this.state.page);
-    }
-
-    getCharacterServices(page){
+    getCharacterServices = (page) => {
         axios.get('https://rickandmortyapi.com/api/character?page=' + page)
         .then( api => {
             console.log(api.data.results)
             this.setState({ characters: api.data.results })
+        })
+        .catch( err => console.log(err));
+    }
+
+    addCharacterServices = (page) => {
+        axios.get('https://rickandmortyapi.com/api/character?page=' + page)
+        .then( api => {
+            console.log(api.data.results)
+            this.setState( prevState => ({ characters: prevState.characters.concat(api.data.results) }))
         })
         .catch( err => console.log(err));
     }
@@ -35,33 +42,57 @@ class CharacterList extends Component {
             this.getCharacterServices(this.state.page);
             console.log(this.state.page)
         })
-
     }
 
     onNextPage = () => {
         this.setState( prevState => ({ page: prevState.page + 1}), () => {
             this.getCharacterServices(this.state.page);
             console.log(this.state.page)
-        })
-
+        });
     }
 
+    onViewMore = () => {
+        this.setState( prevState => ({ page: prevState.page + 1}), () => {
+            this.addCharacterServices(this.state.page);
+            console.log(this.state.page)
+        });
+    }
 
-    render(){
+    onHandleChange = (event) => {
+        this.setState({ text: event.target.value }, () => {
+
+            const data = this.state.characters
+                .filter( item => item.name.toLowerCase().includes(this.state.text.toLowerCase()) );
+
+            this.setState({ search: data});
+        
+        });
+        
+    }
+
+    componentDidMount() {
+        this.getCharacterServices(this.state.page);
+    }
+
+    render() {
+        const { search, text, characters } = this.state;
+
         return(
-            <div>
+            <Fragment>
                 <div>
                     <button onClick={() => this.onBeforePage()}> Atras </button>
+                    <input type="text" onChange={ event => this.onHandleChange(event) } />
                     <button onClick={() => this.onNextPage()}> Adelante </button>
                 </div>
                 <div className="grid">
                     {
-                        this.state.characters.map( 
-                            item => <CharacterItem item={item} />
-                        )
+                        search.length === 0 && text === ''
+                            ? characters.map( item => <CharacterItem item={item} /> )
+                            : search.map( item => <CharacterItem item={item} />  )
                     }
                 </div>
-            </div>
+                <button onClick={() => this.onViewMore()}> Ver Más </button>
+            </Fragment>
         )
     }
     
